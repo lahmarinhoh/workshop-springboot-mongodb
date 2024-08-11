db6cabcf46f8c6b874d5d02cadac3afc54a6ef2c
Lara Marinho Cordeiro <laramaricordeiro@gmail.com>
2024-08-11 18:26:38 -0300

    Insercao de usuario com POST

--- src/main/java/com/laramaricordeiro/workshopmongo/services/UserService.java
+++ src/main/java/com/laramaricordeiro/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.laramaricordeiro.workshopmongo.domain.User;
+import com.laramaricordeiro.workshopmongo.dto.UserDTO;
 import com.laramaricordeiro.workshopmongo.repository.UserRepository;
 import com.laramaricordeiro.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
