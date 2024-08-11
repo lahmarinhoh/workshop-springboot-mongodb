be8951e78e2b396f062a315882b29a9604a655d8
Lara Marinho Cordeiro <laramaricordeiro@gmail.com>
2024-08-11 17:39:32 -0300

    Conectando ao MongoDV com repository e service

---
+++ src/main/java/com/laramaricordeiro/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.laramaricordeiro.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.laramaricordeiro.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+}
