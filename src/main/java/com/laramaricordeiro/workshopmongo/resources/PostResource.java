3a0f037885f365ff6cd9f9cb178a46fe9b2a1de0
Lara Marinho Cordeiro <laramaricordeiro@gmail.com>
2024-08-19 18:17:23 -0300

    Consulta simples com query methods

--- src/main/java/com/laramaricordeiro/workshopmongo/resources/PostResource.java
+++ src/main/java/com/laramaricordeiro/workshopmongo/resources/PostResource.java
@@ -1,18 +1,22 @@
 package com.laramaricordeiro.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.laramaricordeiro.workshopmongo.domain.Post;
+import com.laramaricordeiro.workshopmongo.resources.util.URL;
 import com.laramaricordeiro.workshopmongo.services.PostService;
 
 @RestController
 @RequestMapping(value ="/posts")
-public class PostResourcec {
+public class PostResource {
 	
 	@Autowired
 	private PostService service;
@@ -22,6 +26,14 @@ public class PostResourcec {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);	
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	//ou @GetMapping
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 
 }
